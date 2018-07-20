ebae1d44f6f614c22f9a4ee6e41e14ac9361a355
dagonyclark <sean.dagony-clark@flatironschool.com>
2018-07-20 10:22:33 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
-def square_array(array)
-  # your code here
+require 'pry'
+def square_array(numbers)
+  squared_array = []
+  numbers.each do |square|
+    # binding.pry
+    squared_array.push(square ** 2)
+  end
+  return squared_array
+
 end
+
+# puts [1,2,3]
+# puts square_array([2,4,6])
+# puts square_array([1,2,3])

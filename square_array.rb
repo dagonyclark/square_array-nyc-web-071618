66e5be733535b6685ffd14d87dc433fdb09e2676
dagonyclark <sean@dagony-clark.com>
2019-11-18 11:07:01 -0500

    Revert "Done."
    This reverts commit ebae1d44f6f614c22f9a4ee6e41e14ac9361a355.

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,3 @@
-require 'pry'
-def square_array(numbers)
-  squared_array = []
-  numbers.each do |square|
-    # binding.pry
-    squared_array.push(square ** 2)
-  end
-  return squared_array
-
+def square_array(array)
+  # your code here
 end
-
-# puts [1,2,3]
-# puts square_array([2,4,6])
-# puts square_array([1,2,3])

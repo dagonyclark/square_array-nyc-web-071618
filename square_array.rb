63f67c3eb0f3caf26d87c2ded5e8dc44c4b7f20f
dagonyclark <sean@dagony-clark.com>
2019-11-18 11:12:19 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+  newarray = []
+  array.each do |item|
+    # puts item
+    newarray << item * item
+  end
+  newarray
 end
+
+# square_array([1,2,3])
